0ad14dcf79c146e59bbd4ae5cc2b599d54d60c27
lee-kibugi <Linda.Kibugi@student.Moringa>
2023-10-30 02:33:10 +0300

    completed assignment

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,17 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddTransaction() {
+    const [description, setDescription] = useState(null);
+    const [category, setCategory] = useState(null);
+    const [amount, setAmount] = useState(0);
+    const [date, setDate] = useState(0);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newTransaction = {
+            date,
+            description,
+            category,
+            amount: parseFloat(amount), // Convert amount to a number
+          };
+
+        fetch('http://localhost:4000/transactions', {
+            method: 'POST',
+            headers: {
+                "Content-Type":'application/json'
+            },
+            body: JSON.stringify(newTransaction)
+        })
+        .then((response)=>{
+            if (response.ok) {
+                alert('Transaction added successfully');
+
+                setDescription('');
+                setCategory('');
+                setAmount(0);
+                setDate('');
+              } else {
+                alert('Error adding transaction');
+              } 
+        })
+        .catch (error => {
+            console.error('Error:', error);
+        })
+    }
   return (
     <div>
         <div style={{padding: '10px', fontWeight:'bold', fontSize:'large'}}>Add Transaction</div>
         <form>
             <label htmlFor="description">Description: </label>
             <br />
-            <input type='text' placeholder='Enter description'/>
+            <input 
+            type='text' 
+            required
+            placeholder='Enter description'
+            onChange={(e)=> setDescription(e.target.value)}
+            />
             <br />
             <label htmlFor="description">Category: </label>
             <br />
-            <select>
+            <select
+            required
+            onChange={(e)=> setCategory(e.target.value)}
+
+            >
+                <option></option>
                 <option value="Income">Income</option>
                 <option value="Food">Food</option>
                 <option value="Fashion">Fashion</option>
@@ -24,16 +73,29 @@ function AddTransaction() {
             <br />
             <label htmlFor="Amount">Amount: </label>
             <br />
-            <input type='number' placeholder='Enter amount'/>
+            <input 
+            type='number' 
+            placeholder='Enter amount'
+            required
+            onChange={(e)=> setAmount(e.target.value)}
+            />
              
             <br />
             <label htmlFor="date">Select Date: </label>
             <br />
-            <input type="date" />
+            <input 
+            type="date"
+            onChange={(e)=> setDate(e.target.value)}
+            required
+            />
 
             <br />
 
-            <button type='submit'>
+            <button
+            onClick={
+                (e)=> handleSubmit(e)
+            }
+            type='submit'>
                 Submit
             </button>
 
